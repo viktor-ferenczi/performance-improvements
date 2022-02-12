9dc8ad6957a6e3fe6d9b315aa7a6ab9b475cb14d
Viktor Ferenczi <viktor@ferenczi.eu>
2022-02-12 03:08:22 +0100

    Wording fixes

--- ClientPlugin/GUI/MyPluginConfigDialog.cs
+++ ClientPlugin/GUI/MyPluginConfigDialog.cs
@@ -84,7 +84,7 @@ namespace ClientPlugin.GUI
             CreateCheckbox(out fixGridPasteLabel, out fixGridPasteCheckbox, config.FixGridPaste, value => config.FixGridPaste = value, "Fix grid paste", "Disables updates during grid paste (MyCubeGrid.PasteBlocksServer)");
             CreateCheckbox(out fixP2PUpdateStatsLabel, out fixP2PUpdateStatsCheckbox, config.FixP2PUpdateStats, value => config.FixP2PUpdateStats = value, "Fix P2P update stats", "Eliminates 98% of EOS P2P network statistics updates (VRage.EOS.MyP2PQoSAdapter.UpdateStats)");
             CreateCheckbox(out fixGarbageCollectionLabel, out fixGarbageCollectionCheckbox, config.FixGarbageCollection, value => config.FixGarbageCollection = value, "Disables GC.Collect calls", "Disables selected GC.Collect calls, which may cause long pauses on starting and stopping large worlds");
-            CreateCheckbox(out fixThrustersLabel, out fixThrustersCheckbox, config.FixThrusters, value => config.FixThrusters = value, "Fix Thruster Updates", "Throttles thruster grid updates to only happen once a second");
+            CreateCheckbox(out fixThrustersLabel, out fixThrustersCheckbox, config.FixThrusters, value => config.FixThrusters = value, "Fix thrusters", "Throttles the maximum thrust calculation to happen only once a second");
             CreateCheckbox(out disableModApiStatisticsLabel, out disableModApiStatisticsCheckbox, config.DisableModApiStatistics, value => config.DisableModApiStatistics = value, "Disables Mod API statistics", "Disables the collection of Mod API call statistics to eliminate the overhead (affects only world loading)");
 
             EnableDisableFixes();
