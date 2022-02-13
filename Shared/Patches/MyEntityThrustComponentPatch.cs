003de6d6b86ef6c581de6f7abe755b4e333d3e7b
Viktor Ferenczi <viktor@ferenczi.eu>
2022-02-14 00:13:34 +0100

    Working thruster fix

--- Shared/Patches/MyEntityThrustComponentPatch.cs
+++ Shared/Patches/MyEntityThrustComponentPatch.cs
@@ -1,10 +1,9 @@
-﻿#if BROKEN
-
-using HarmonyLib;
+﻿using HarmonyLib;
 using Sandbox.Game.Entities;
 using Sandbox.Game.GameSystems;
 using Shared.Config;
 using Shared.Plugin;
+using VRageMath;
 
 namespace Shared.Patches
 {
@@ -14,13 +13,23 @@ namespace Shared.Patches
     {
         private static IPluginConfig Config => Common.Config;
 
+        // Latest actioned ControlThrust values
+        // XYZ values are between -1..+1
+        private const int Count = 1 << 12;
+        private const int Mask = Count - 1;
+        private static readonly Vector3[] LatestActionedControlThrusts = new Vector3[Count];
+
+        // Save the recalculation below this change threshold (make it configurable if needed)
+        private const double ChangeThreshold = 0.01;
+
         // ReSharper disable once UnusedMember.Local
-        [HarmonyPrefix]
-        [HarmonyPatch("RecomputeThrustParameters")]
         // ReSharper disable once InconsistentNaming
-        private static bool RecomputeThrustParametersPrefix(MyEntityThrustComponent __instance)
+        [HarmonyPrefix]
+        [HarmonyPatch("UpdateBeforeSimulation")]
+        private static bool UpdateBeforeSimulationPrefix(MyEntityThrustComponent __instance, ref bool ___m_thrustsChanged)
         {
-            if (MyCharacterJetpackComponentPatch.IsInTurnOnJetpack)
+            // Would the recalculation run normally?
+            if (!___m_thrustsChanged)
                 return true;
 
             if (!Config.Enabled || !Config.FixThrusters)
@@ -29,9 +38,46 @@ namespace Shared.Patches
             if (!(__instance.Entity is MyCubeGrid grid))
                 return true;
 
-            return grid.EntityId % 60 == Common.Plugin.Tick % 60;
+            // Latest control thrust is saved up to Count grids based on a hash,
+            // which allows for an algorithm which does not allocate any memory.
+            // Hash collisions may cause slightly more recalculations, because
+            // they overwrite each other's latest control thrust vectors causing
+            // large differences all the time. It should happen only rarely.
+
+            // Hash is just the lower bits of EntityId, because it is already random
+            var hash = (int)(grid.EntityId & Mask);
+            var latestControlThrust = LatestActionedControlThrusts[hash];
+
+            // Always allow for zeroing the control thrust
+            var currentControlThrust = __instance.ControlThrust;
+            if (currentControlThrust == Vector3.Zero)
+            {
+                if (latestControlThrust == Vector3.Zero)
+                {
+                    // Suppress the RecomputeThrustParameters calls in the original method
+                    ___m_thrustsChanged = false;
+                    return true;
+                }
+
+                // Allow calling RecomputeThrustParameters,
+                // record that it was called for the zero control vector
+                LatestActionedControlThrusts[hash] = Vector3.Zero;
+                return true;
+            }
+
+            // Suppress the recomputation if the change in the control vector is small
+            var change = currentControlThrust - latestControlThrust;
+            if (change.AbsMax() < ChangeThreshold)
+            {
+                // Suppress the RecomputeThrustParameters calls in the original method
+                ___m_thrustsChanged = false;
+                return true;
+            }
+
+            // Allow calling RecomputeThrustParameters,
+            // record that it was called for the current control vector
+            LatestActionedControlThrusts[hash] = currentControlThrust;
+            return true;
         }
     }
 }
-
-#endif
--- TorchPlugin/PluginConfig.cs
+++ TorchPlugin/PluginConfig.cs
@@ -19,56 +19,56 @@ namespace TorchPlugin
         //BOOL_OPTION private bool optionName = true;
         private bool disableModApiStatistics = true;
 
-        [Display(Order = 1, GroupName = "General", Name = "Enable plugin", Description = "Enables/disables the plugin")]
+        [Display(Order = 1, GroupName = "General", Name = "Enable plugin", Description = "Enable the plugin (all fixes)")]
         public bool Enabled
         {
             get => enabled;
             set => SetValue(ref enabled, value);
         }
 
-        [Display(Order = 2, GroupName = "Fixes", Name = "Spin lock", Description = "Enables the MySpinWait.SpinOnce fix")]
+        [Display(Order = 2, GroupName = "Fixes", Name = "Spin lock", Description = "Alternative spin wait algorithm to reduce CPU load (MySpinWait.SpinOnce)")]
         public bool FixSpinWait
         {
             get => fixSpinWait;
             set => SetValue(ref fixSpinWait, value);
         }
 
-        [Display(Order = 3, GroupName = "Fixes", Name = "Grid merge", Description = "Enables the MyCubeGrid.MergeGridInternal fix")]
+        [Display(Order = 3, GroupName = "Fixes", Name = "Grid merge", Description = "Disable conveyor updates during grid merge (MyCubeGrid.MergeGridInternal)")]
         public bool FixGridMerge
         {
             get => fixMerge;
             set => SetValue(ref fixMerge, value);
         }
 
-        [Display(Order = 4, GroupName = "Fixes", Name = "Grid paste", Description = "Enables the MyCubeGrid.PasteBlocksServer fix")]
+        [Display(Order = 4, GroupName = "Fixes", Name = "Grid paste", Description = "Disable updates during grid paste (MyCubeGrid.PasteBlocksServer)")]
         public bool FixGridPaste
         {
             get => fixPast;
             set => SetValue(ref fixPast, value);
         }
 
-        [Display(Order = 5, GroupName = "Fixes", Name = "P2P stats update", Description = "Enables the VRage.EOS.MyP2PQoSAdapter.UpdateStats fix")]
+        [Display(Order = 5, GroupName = "Fixes", Name = "P2P stats update", Description = "Eliminate 98% of EOS P2P network statistics updates (VRage.EOS.MyP2PQoSAdapter.UpdateStats)")]
         public bool FixP2PUpdateStats
         {
             get => fixUpdateStat;
             set => SetValue(ref fixUpdateStat, value);
         }
 
-        [Display(Order = 6, GroupName = "Fixes", Name = "Fix garbage collection", Description = "Eliminates long pauses on starting and stopping large worlds by disabling selected GC.Collect calls")]
+        [Display(Order = 6, GroupName = "Fixes", Name = "Fix garbage collection", Description = "Eliminate long pauses on starting and stopping large worlds by disabling selected GC.Collect calls")]
         public bool FixGarbageCollection
         {
             get => fixGarbageCollection;
             set => SetValue(ref fixGarbageCollection, value);
         }
 
-        [Display(Order = 7, GroupName = "Fixes", Name = "DISABLED: Fix thrusters", Description = "BROKEN: This fix is disabled regardless of your setting until it is fixed.")]
+        [Display(Order = 7, GroupName = "Fixes", Name = "Fix thrusters", Description = "Recalculate thrust parameters only after relevant change in the control thrust")]
         public bool FixThrusters
         {
             get => fixThrusters;
             set => SetValue(ref fixThrusters, value);
         }
 
-        [Display(Order = 9, GroupName = "Fixes", Name = "Fix grid groups", Description = "Disables resource updates while grids are being moved between groups")]
+        [Display(Order = 9, GroupName = "Fixes", Name = "Fix grid groups", Description = "Disable resource updates while grids are being moved between groups")]
         public bool FixGridGroups
         {
             get => fixGridGroups;
@@ -84,7 +84,7 @@ namespace TorchPlugin
         }
 
         BOOL_OPTION*/
-        [Display(Order = 19, GroupName = "Fixes", Name = "Disables Mod API statistics", Description = "Disables the collection of Mod API call statistics to eliminate the overhead")]
+        [Display(Order = 19, GroupName = "Fixes", Name = "Disable Mod API statistics", Description = "Disable the collection of Mod API call statistics to eliminate the overhead")]
         public bool DisableModApiStatistics
         {
             get => disableModApiStatistics;
