6b5e9f15c5fe8abff14af33458987b4e4672ad0c
Viktor Ferenczi <viktor@ferenczi.eu>
2022-03-23 01:22:56 +0100

    Fixed race condition in MySafeZone.RemoveEntityPhantom

--- Shared/Patches/SafeZone/MySafeZonePatch.cs
+++ Shared/Patches/SafeZone/MySafeZonePatch.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 using HarmonyLib;
 using Sandbox.Game.Entities;
 using Shared.Config;
 using Shared.Plugin;
 using Shared.Tools;
 using VRage.Game.Entity;
+using VRage.Game.ModAPI.Ingame;
 
 namespace Shared.Patches
 {
@@ -21,11 +24,16 @@ namespace Shared.Patches
 
         static MySafeZonePatch()
         {
-            UpdateEnabled();
+            Configure();
             Config.PropertyChanged += OnConfigChanged;
         }
 
-        private static void UpdateEnabled()
+        private static void OnConfigChanged(object sender, PropertyChangedEventArgs e)
+        {
+            Configure();
+        }
+
+        public static void Configure()
         {
             enabled = Config.Enabled && Config.FixSafeZone;
 
@@ -33,10 +41,7 @@ namespace Shared.Patches
                 cache.Clear();
         }
 
-        private static void OnConfigChanged(object sender, PropertyChangedEventArgs e)
-        {
-            UpdateEnabled();
-        }
+        #region "IsSafe fix, see: https://support.keenswh.com/spaceengineers/pc/topic/24146-performance-mysafezone-issafe-is-called-frequently-but-not-cached"
 
         private class CachedResult
         {
@@ -116,5 +121,68 @@ namespace Shared.Patches
 
             cache[entityId] = new CachedResult { Expires = expires, Result = __result };
         }
+
+        #endregion
+
+        #region "RemoveEntityPhantom fix, see: https://support.keenswh.com/spaceengineers/pc/topic/24149-safezone-m_removeentityphantomtasklist-hashset-corruption-due-to-race-condition"
+
+        [HarmonyTranspiler]
+        [HarmonyPatch("RemoveEntityPhantom")]
+        [EnsureCode("55db36e5")]
+        // ReSharper disable once UnusedMember.Local
+        private static IEnumerable<CodeInstruction> RemoveEntityPhantomTranspiler(IEnumerable<CodeInstruction> instructions)
+        {
+            if (!enabled)
+                return instructions;
+
+            var il = instructions.ToList();
+
+            var i = il.FindIndex(ci => ci.opcode == OpCodes.Callvirt && ci.operand is MethodInfo mi && mi.Name.Contains("Contains"));
+            il[i] = new CodeInstruction(OpCodes.Call, AccessTools.DeclaredMethod(typeof(MySafeZonePatch), nameof(PhantomTaskListContains)));
+
+            // Lock around the Add call
+            var j = il.FindIndex(ci => ci.opcode == OpCodes.Callvirt && ci.operand is MethodInfo mi && mi.Name.Contains("Add"));
+            il[j] = new CodeInstruction(OpCodes.Call, AccessTools.DeclaredMethod(typeof(MySafeZonePatch), nameof(PhantomTaskListAdd)));
+
+            return il.AsEnumerable();
+        }
+
+        [HarmonyTranspiler]
+        [HarmonyPatch("Sandbox.Game.Entities.MySafeZone+<>c__DisplayClass103_0", "<RemoveEntityPhantom>b__0")]
+        [EnsureCode("b39ccae8")]
+        // ReSharper disable once UnusedMember.Local
+        private static IEnumerable<CodeInstruction> RemoveEntityPhantomClosureTranspiler(IEnumerable<CodeInstruction> instructions)
+        {
+            if (!enabled)
+                return instructions;
+
+            var il = instructions.ToList();
+
+            // Lock around the Remove call
+            var i = il.FindIndex(ci => ci.opcode == OpCodes.Callvirt && ci.operand is MethodInfo mi && mi.Name.Contains("Remove"));
+            il[i] = new CodeInstruction(OpCodes.Call, AccessTools.DeclaredMethod(typeof(MySafeZonePatch), nameof(PhantomTaskListRemove)));
+
+            return il.AsEnumerable();
+        }
+
+        private static bool PhantomTaskListContains(HashSet<IMyEntity> map, IMyEntity item)
+        {
+            lock(map)
+                return map.Contains(item);
+        }
+
+        private static bool PhantomTaskListAdd(HashSet<IMyEntity> map, IMyEntity item)
+        {
+            lock (map)
+                return map.Add(item);
+        }
+
+        private static bool PhantomTaskListRemove(HashSet<IMyEntity> map, IMyEntity item)
+        {
+            lock (map)
+                return map.Remove(item);
+        }
+
+        #endregion
     }
 }
