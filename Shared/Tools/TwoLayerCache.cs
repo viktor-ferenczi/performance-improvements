7e8cb0963d21a18d7de696cf184da9601649be5b
Viktor Ferenczi <viktor@ferenczi.eu>
2023-04-15 04:08:48 +0200

    Cache fixes

--- Shared/Tools/TwoLayerCache.cs
+++ Shared/Tools/TwoLayerCache.cs
@@ -1,10 +1,16 @@
+#if DEBUG
+
+// Uncomment to enable collecting statistics
+#define COLLECT_STATS
+
+#endif
+
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using Shared.Tools;
 
-namespace TorchPlugin.Shared.Tools
+namespace Shared.Tools
 {
-    public class CacheForever<TK, TV>
+    public class TwoLayerCache<TK, TV>
     {
         // First layer immutable cache, which does not need synchronization, but needs to be filled
         private IReadOnlyDictionary<TK, TV> immutableCache = new Dictionary<TK, TV>();
@@ -12,18 +18,25 @@ namespace TorchPlugin.Shared.Tools
         // Second layer mutable and synchronized cache, collecting new keys
         private readonly RwLockDictionary<TK, TV> cache = new RwLockDictionary<TK, TV>();
 
-#if DEBUG
-        public readonly CacheStat ImmutableStat = new CacheStat();
-        public readonly CacheStat Stat = new CacheStat();
-        public string ImmutableReport => ImmutableStat.Report;
-        public string Report => Stat.Report;
+#if COLLECT_STATS
+        private readonly CacheStat immutableStat = new CacheStat();
+        private readonly CacheStat stat = new CacheStat();
+        public string ImmutableReport => immutableStat.Report;
+        public string Report => stat.Report;
 #endif
 
         public void Clear()
         {
             cache.BeginWriting();
+            
             immutableCache = new Dictionary<TK, TV>();
             cache.Clear();
+
+#if COLLECT_STATS
+            immutableStat.Clear();
+            stat.Clear();
+#endif
+            
             cache.FinishWriting();
         }
 
@@ -33,7 +46,13 @@ namespace TorchPlugin.Shared.Tools
                 return;
 
             cache.BeginReading();
+            
             immutableCache = new Dictionary<TK, TV>(cache);
+            
+#if COLLECT_STATS
+            immutableStat.Reset(immutableCache.Count);
+#endif
+            
             cache.FinishReading();
         }
 
@@ -56,28 +75,28 @@ namespace TorchPlugin.Shared.Tools
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public bool TryGetValue(TK key, out TV value)
         {
-#if DEBUG
-            ImmutableStat.CountLookup(immutableCache.Count);
+#if COLLECT_STATS
+            immutableStat.CountLookup(immutableCache.Count);
 #endif
 
             if (immutableCache.TryGetValue(key, out value))
             {
-#if DEBUG
-                ImmutableStat.CountHit();
+#if COLLECT_STATS
+                immutableStat.CountHit();
 #endif
                 return true;
             }
 
-#if DEBUG
-            Stat.CountLookup(cache.Count);
+#if COLLECT_STATS
+            stat.CountLookup(cache.Count);
 #endif
 
             cache.BeginReading();
             if (cache.TryGetValue(key, out value))
             {
                 cache.FinishReading();
-#if DEBUG
-                Stat.CountHit();
+#if COLLECT_STATS
+                stat.CountHit();
 #endif
                 return true;
             }
