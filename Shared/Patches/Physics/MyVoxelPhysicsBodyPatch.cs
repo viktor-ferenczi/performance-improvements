658f611c59fe71db7fb9326134e01cb1cd04fca4
Viktor Ferenczi <viktor@ferenczi.eu>
2023-09-06 19:45:59 +0200

    Re-enabled MyVoxelPhysicsBodyPatch fix

--- Shared/Patches/Physics/MyVoxelPhysicsBodyPatch.cs
+++ Shared/Patches/Physics/MyVoxelPhysicsBodyPatch.cs
@@ -5,14 +5,17 @@ using Sandbox.Game.Entities;
 namespace Shared.Patches
 {
     [HarmonyPatch(typeof(MyVoxelPhysicsBody))]
+    // ReSharper disable once UnusedType.Global
     public static class MyVoxelPhysicsBodyPatch
     {
         [HarmonyPrefix]
         [HarmonyPatch(MethodType.Constructor, typeof(MyVoxelBase), typeof(float), typeof(float), typeof(bool))]
+        // ReSharper disable once InconsistentNaming
+        // ReSharper disable once RedundantAssignment
         private static bool ConstructorPrefix(ref bool ___m_staticForCluster)
         {
             // Reverting to the 1.202 default
-            // ___m_staticForCluster = true;
+            ___m_staticForCluster = true;
 
             return true;
         }
