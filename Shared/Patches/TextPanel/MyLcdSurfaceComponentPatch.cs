32643384f9df3d15a53f7be57a8a9e76ec970b18
Viktor Ferenczi <viktor@ferenczi.eu>
2023-09-13 00:00:38 +0200

    MyLcdSurfaceComponentPatch.UpdateHideableScreenVisibilityPrefix patch (Torch and DS only)

---
+++ Shared/Patches/TextPanel/MyLcdSurfaceComponentPatch.cs
@@ -0,0 +1,25 @@
+﻿#if !CLIENT
+
+using HarmonyLib;
+using Shared.Config;
+using Shared.Plugin;
+using Shared.Tools;
+
+namespace Shared.Patches.TextPanel
+{
+    [HarmonyPatch]
+    public static class MyLcdSurfaceComponentPatch
+    {
+        private static IPluginConfig Config => Common.Config;
+
+        [HarmonyPrefix]
+        [HarmonyPatch("IsInAtmosphere", MethodType.Getter)]
+        [EnsureCode("xxx")]
+        private static bool UpdateHideableScreenVisibilityPrefix()
+        {
+            return !Config.FixTextPanel;
+        }
+    }
+}
+
+#endif
