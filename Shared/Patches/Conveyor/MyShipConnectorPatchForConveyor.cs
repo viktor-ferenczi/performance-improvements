06716fe0329a37c14aa031e95e5aeae28162670b
Viktor Ferenczi <viktor@ferenczi.eu>
2023-09-15 19:19:17 +0200

    Invalidating the Reachable cache on mechanical connection attach/detach and connector lock/unlock

---
+++ Shared/Patches/Conveyor/MyShipConnectorPatchForConveyor.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using HarmonyLib;
+using Sandbox.Game.Entities.Cube;
+using Shared.Config;
+using Shared.Plugin;
+using Shared.Tools;
+
+namespace Shared.Patches
+{
+    [SuppressMessage("ReSharper", "UnusedMember.Local")]
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    [HarmonyPatch(typeof(MyShipConnector))]
+    [SuppressMessage("ReSharper", "UnusedType.Global")]
+    public static class MyShipConnectorPatchForConveyor
+    {
+        private static IPluginConfig Config => Common.Config;
+
+        [HarmonyPatch("OnConnectionStateChanged")]
+        [HarmonyPostfix]
+        [EnsureCode("5c9de4df")]
+        private static void OnConnectionStateChangedPostfix(MyShipConnector __instance)
+        {
+            if (Config.FixConveyor)
+            {
+                var grid = __instance.CubeGrid;
+                if (grid != null)
+                {
+                    MyGridConveyorSystemPatch.DropCache(grid);
+                }
+            }
+        }
+    }
+}
