7af270353331bd743a92a1f6b84e491d75a156f9
Viktor Ferenczi <viktor@ferenczi.eu>
2024-10-14 19:45:55 +0200

    Wrapped hash calculations in unchecked

--- Shared/Tools/Hashing.cs
+++ Shared/Tools/Hashing.cs
@@ -44,21 +44,24 @@ namespace Shared.Tools
         {
             // https://stackoverflow.com/questions/1646807/quick-and-simple-hash-code-combinations
 
-            var hash1 = (5381 << 16) + 5381;
-            var hash2 = hash1;
-
-            var i = 0;
-            foreach (var hashCode in hashCodes)
+            unchecked
             {
-                if (i % 2 == 0)
-                    hash1 = ((hash1 << 5) + hash1 + (hash1 >> 27)) ^ hashCode;
-                else
-                    hash2 = ((hash2 << 5) + hash2 + (hash2 >> 27)) ^ hashCode;
+                var hash1 = (5381 << 16) + 5381;
+                var hash2 = hash1;
 
-                ++i;
+                var i = 0;
+                foreach (var hashCode in hashCodes)
+                {
+                    if ((i & 1) == 0)
+                        hash1 = ((hash1 << 5) + hash1 + (hash1 >> 27)) ^ hashCode;
+                    else
+                        hash2 = ((hash2 << 5) + hash2 + (hash2 >> 27)) ^ hashCode;
+
+                    ++i;
+                }
+
+                return hash1 + hash2 * 1566083941;
             }
-
-            return hash1 + hash2 * 1566083941;
         }
     }
 }
